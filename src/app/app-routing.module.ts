90963a8a2e3df1dbb3755741cd04daef90c962d4
Simon Moreau <simon@bim42.com>
2019-02-22 21:50:44 +0100

    add google charts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { GraphComponent } from './graph/graph.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-{path: 'home', component: HomeComponent}];
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'graph', component: GraphComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MarkupComponent } from './markup/markup.component';
+import { GraphComponent } from './graph/graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MarkupComponent
+    MarkupComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
